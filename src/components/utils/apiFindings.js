ab9dd883e47f5091fb0e8be489d9096497438a73
1alron <roma.niko393@gmail.com>
2024-12-08 10:54:29 +0300

    init

---
+++ src/components/utils/apiFindings.js
@@ -0,0 +1,10 @@
+// Объект для передачи(получаения) данных на сервер(сервера)
+const apiFindings = {
+  link: 'https://mesto.nomoreparties.co/v1/frontend-st-cohort-201/',
+  headers: {
+    authorization: '40f4a4b4-eac4-46f9-a6b5-a8655d83f62f',
+    'Content-Type': 'application/json'
+  }
+}
+// Экспортируем объект в index.js
+export { apiFindings };
